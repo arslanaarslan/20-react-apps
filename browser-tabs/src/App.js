f0313b43bd7a5b37adce2621df4302d699b0c34f
Arslan Arslan <arslanaarslan@gmail.com>
2023-10-14 22:23:59 +0300

    browser-tabs added

---
+++ browser-tabs/src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import React, { useState } from 'react';
+import Tab from './components/Tab';
+import './App.css';
+
+function App() {
+
+
+  return (
+    <div className="app">
+      <div className="browser">
+        <div className="tabs">
+          <Tab>
+            <a>Home</a>
+          </Tab>
+          <Tab>
+            <a>About</a>
+          </Tab>
+          <Tab>
+            <a>Features</a>
+          </Tab>
+        </div>
+
+        <div className="viewport">Pages Go Here</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
