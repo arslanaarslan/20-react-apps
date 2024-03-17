d5276c5528118be12163b47cc80d4716969faa5d
Arslan Arslan <arslanaarslan@gmail.com>
2024-03-17 22:02:28 +0300

    Refactor trivia app to incorporate category selection for questions

--- trivia/src/App.js
+++ trivia/src/App.js
@@ -1,24 +1,25 @@
-import React, {useEffect, useState} from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import Question from './components/Question';
 import CategorySelector from './components/CategorySelector';
-import ResultModal from './components/ResultModal';
 import Scoreboard from './components/Scoreboard';
 import './App.css';
 
 export default function App() {
     const [question, setQuestion] = useState(null);
+    const [selectedCategory, setSelectedCategory] = useState('any');
 
-    useEffect(() => {
-        getQuestion();
-    }, []);
-
-    function getQuestion() {
-        const url = "https://opentdb.com/api.php?amount=1";
+    const getQuestion = useCallback(() => {
+        let url = "https://opentdb.com/api.php?amount=1";
+        if (selectedCategory !== 'any') url += `&category=${selectedCategory}`;
 
         fetch(url)
             .then((res) => res.json())
             .then((data) => setQuestion(data.results?.[0]));
-    }
+    }, [selectedCategory]);
+
+    useEffect(() => {
+        getQuestion();
+    }, [getQuestion, selectedCategory]);
 
     return (
       <div className="app">
@@ -27,7 +28,10 @@ export default function App() {
 
         {/* question header ----------------------- */}
         <div className="question-header">
-          <CategorySelector />
+          <CategorySelector
+              category={selectedCategory}
+              chooseCategory={setSelectedCategory}
+          />
           <Scoreboard />
         </div>
 
